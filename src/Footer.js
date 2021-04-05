c5ce51499461d00de793fc49f102f66e7a36ee0e
Lotz33 <55788592+Lotz33@users.noreply.github.com>
2021-04-05 17:43:03 +0100

    updated github link

--- src/Footer.js
+++ src/Footer.js
@@ -3,10 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="card-footer">
-      Coded by{" "}
-      <a href="https://github.com/Lotz33/Weather-Test/blob/main/index.html">
-        Lottie
-      </a>
+      Coded by <a href="https://github.com/Lotz33/weather-react">Lottie</a>
     </div>
   );
 }
