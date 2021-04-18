b05628e4e4cdb91fd5ed9de6381fa743fba2922b
Lotz33 <55788592+Lotz33@users.noreply.github.com>
2021-04-18 17:53:22 +0100

    convert C & F degrees

--- src/Temp.js
+++ src/Temp.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Temp(props) {
-  return (
-    <div>
-      <span className="card-title" id="degrees">
-        {Math.round(props.celsius)}
-      </span>
-      <span id="units">
-        <a href="#0" id="celsius-link" className="active">
-          ℃
-        </a>{" "}
-        |
-        <a href="#0" id="fahrenheit-link">
-          ℉
-        </a>
-      </span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+  function showF(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function showC(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div>
+        <span className="card-title" id="degrees">
+          {Math.round(props.celsius)}
+        </span>
+        <span className="unit">
+          ℃ |
+          <a href="/" onClick={showF}>
+            ℉
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <div>
+        <span className="card-title" id="degrees">
+          {Math.round(fahrenheit)}
+        </span>
+        <span className="unit">
+          <a href="/" onClick={showC}>
+            ℃{" "}
+          </a>
+          | ℉
+        </span>
+      </div>
+    );
+  }
 }
